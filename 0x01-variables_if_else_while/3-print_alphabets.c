6c3dcb17e654d3e7d53fd4e8fe6ffe1587c5cbbe
Nourgamal425 <nourhangamal425@yahoo.com>
2023-04-21 02:12:47 -0700

    alphABET task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+	int a = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	while (a <= 90)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
